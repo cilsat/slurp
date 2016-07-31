5dbbf3cdc45e63c29ebd771a89e3f763b9fe75bf
Dimas <dimas.gs@outlook.com>
2016-07-31 19:07:51 +0700

    remove deprecated

--- ellipsoid.py
+++ ellipsoid.py
@@ -1,22 +1,7 @@
 import math
 import numpy as np
 
-def calc_z(cx, cy, rh, rv, x, y):
-    """
-    Keyword arguments:
-    cx, cy -- center x, center y
-    rh, rv -- radius horizontal, radius vertical
-    x, y -- pos where to calculate z
-    """
-    x_2, y_2 = math.pow(x-cx, 2), math.pow(y-cy, 2)
-    rh_2, rv_2 = math.pow(rh, 2), math.pow(rv, 2)
-    z_2 = float(rh_2-x_2-y_2)*rv_2/rh_2
-    if z_2 < 0:
-        return float('nan')
-    else:
-        return math.sqrt(z_2)
-
-def calc_z_vectorized(cx, cy, rh, rv, gridxy):
+def calc_z(cx, cy, rh, rv, gridxy):
     rh_2, rv_2 = rh*rh, rv*rv
 
     _gridxy = gridxy.astype(np.float, copy=True)
